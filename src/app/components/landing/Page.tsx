0bddb620629ddf2d273a899923864bbb12fceb72
Danny Nguyen <ndanny@github.com>
2024-08-21 14:48:55 -0700

    Add photo to landing page

--- src/app/components/landing/Page.tsx
+++ src/app/components/landing/Page.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import Image from "next/image";
 
 const LandingPage = () => {
   return (
-    <div className="p-10">
-      Temp
+    <div className="">
+      <div className="relative h-[800px] w-full overflow-hidden">
+        <div
+          className="absolute inset-0"
+          style={{ top: "-100px", height: "calc(100% + 100px)" }}
+        >
+          <Image
+            src="/TeslaStretchCompressed.jpg"
+            alt="Tesla car"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="top"
+            priority
+          />
+        </div>
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center pt-20">
+          <p className="text-sm text-white mb-2">#1 Auto shop in Sacramento</p>
+          <h1 className="text-6xl font-bold text-white text-center mb-4">
+            Protect Your Vehicle
+          </h1>
+          <p className="text-sm text-white mb-4">
+            PPF - WRAP - CERAMIC COATING - TINT
+          </p>
+          <button className="bg-blue-800 mb-4 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded">
+            Get Pricing Now
+          </button>
+          <p className="text-sm text-white mb-4">Elk Grove, CA</p>
+        </div>
+      </div>
+      <div className="h-[500px]" />
       {/* Have a stock image and text above to show a car they have odne as well as showing their name */}
       {/* About Us section */}
       {/* All services they provide, each having its own card, cards contain image and text */}
