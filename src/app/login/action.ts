85d4d16012e25347a577bac3b09a89d16f8934a4
Steven Ho <steven09ho@gmail.com>
2024-10-31 16:41:12 -0700

    supa update

--- src/app/login/action.ts
+++ src/app/login/action.ts
@@ -1,46 +1,47 @@
-// "use server";
+"use server";
 
-// import { revalidatePath } from "next/cache";
-// import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers"; // Make sure to import cookies
 
-// import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/server";
 
-// export async function login(formData: FormData) {
-//   const supabase = createClient();
+export async function login(formData: FormData) {
+  const cookieStore = cookies(); // Get the cookie store
+  const supabase = createClient(cookieStore); // Pass it to createClient
 
-//   // type-casting here for convenience
-//   // in practice, you should validate your inputs
-//   const data = {
-//     email: formData.get("email") as string,
-//     password: formData.get("password") as string,
-//   };
+  // Type-casting here for convenience
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
 
-//   const { error } = await supabase.auth.signInWithPassword(data);
+  const { error } = await supabase.auth.signInWithPassword(data);
 
-//   if (error) {
-//     redirect("/error");
-//   }
+  if (error) {
+    redirect("/error");
+  }
 
-//   revalidatePath("/", "layout");
-//   redirect("/account");
-// }
+  revalidatePath("/", "layout");
+  redirect("/account");
+}
 
-// export async function signup(formData: FormData) {
-//   const supabase = createClient();
+export async function signup(formData: FormData) {
+  const cookieStore = cookies(); // Get the cookie store
+  const supabase = createClient(cookieStore); // Pass it to createClient
 
-//   // type-casting here for convenience
-//   // in practice, you should validate your inputs
-//   const data = {
-//     email: formData.get("email") as string,
-//     password: formData.get("password") as string,
-//   };
+  // Type-casting here for convenience
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
 
-//   const { error } = await supabase.auth.signUp(data);
+  const { error } = await supabase.auth.signUp(data);
 
-//   if (error) {
-//     redirect("/error");
-//   }
+  if (error) {
+    redirect("/error");
+  }
 
-//   revalidatePath("/", "layout");
-//   redirect("/account");
-// }
+  revalidatePath("/", "layout");
+  redirect("/account");
+}
